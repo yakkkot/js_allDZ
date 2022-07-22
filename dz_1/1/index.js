46eadf89584a218d2517c8317f603d08070137ec
yakkkot <k0tjeee777@gmail.com>
2022-07-22 16:29:16 +0300

    Initial commit

---
+++ dz_1/1/index.js
@@ -0,0 +1,80 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let hello = 'hello';
+
+console.log(hello);
+alert(hello);
+document.write(`<h1> ${hello} </h1>`);
+
+let owu = 'owu';
+console.log(owu);
+alert(owu);
+document.write(`<h1> ${owu} </h1>`);
+
+let com = 'com';
+console.log(com);
+alert(com);
+document.write(`<h1> ${com} </h1>`);
+
+let ua = 'ua';
+console.log(ua);
+alert(ua);
+document.write(`<h1> ${ua} </h1>`);
+
+let x = 1;
+console.log(x);
+alert(x);
+document.write(`<h1> ${x} </h1>`);
+
+let y = 10;
+console.log(y);
+alert(y);
+document.write(`<h1> ${y} </h1>`);
+
+let z = -999;
+console.log(z);
+alert(z);
+document.write(`<h1> ${z} </h1>`);
+
+let aa = 123;
+console.log(aa);
+alert(aa);
+document.write(`<h1> ${aa} </h1>`);
+
+let pi = 3.14;
+console.log(pi);
+alert(pi);
+document.write(`<h1> ${pi} </h1>`);
+
+let bb = 2.7;
+console.log(bb);
+alert(bb);
+document.write(`<h1> ${bb} </h1>`);
+
+let cc = 16;
+console.log(cc);
+alert(cc);
+document.write(`<h1> ${cc} </h1>`);
+
+let tr = true;
+console.log(tr);
+alert(tr);
+document.write(`<h1> ${tr} </h1>`);
+
+let fal = false;
+console.log(fal);
+alert(fal);
+document.write(`<h1> ${fal} </h1>`);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let firstName = 'Yaroslav';
+let middleName = 'Yuriyovych';
+let lastName = 'Kot';
+let person1 = `${firstName} ${middleName} ${lastName}`;
+console.log(person1);
+let person2 = firstName + ' ' + middleName + ' '+ lastName;
+console.log(person2);
+
+
+
---
+++ dz_1/2/index.js
@@ -0,0 +1,14 @@
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let name = prompt(`Your name`);
+let secondName = prompt(`Your second name`);
+let year = prompt(`How old are you?`);
+console.log(name, secondName, +year);
